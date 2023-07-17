b407c9d839a1e5272e2d5d0b2ed9ffd76029c2d0
Mudit Arora <44677855+Mudit-Arora@users.noreply.github.com>
2023-07-17 12:49:55 -0700

    Final Commit

--- backend.py
+++ backend.py
@@ -7,9 +7,9 @@ def get_data(place, days=None):
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
-    nr_values = 8*days
+    nr_values = 8 * days
     filtered_data = filtered_data[:nr_values]
     return filtered_data
 
-if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+# if __name__ == "__main__":
+#    print(get_data(place="Tokyo"))
